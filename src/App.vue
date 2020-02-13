59ef291eea02cd3bef5d84ea3e404c075741bba7
KhadijeElzein <khady_elzein@hotmail.com>
2020-02-12 21:27:08 -0300

    boiler plate code with some configurations set
    For instance, the main.js references routes and App has an router-view element.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="body">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#body {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+  background:#404040;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(VueResource)
+Vue.http.options.root = 'https://moviewebappbackend.herokuapp.com/'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
