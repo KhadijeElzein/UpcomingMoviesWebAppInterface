6e5d0626007e07ed14165d6249854874ce90c4c6
KhadijeElzein <khady_elzein@hotmail.com>
2020-02-12 21:24:24 -0300

    created reusable components

---
+++ src/components/shared/header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="danger" class="header">
+    <b-navbar-brand href="#">Upcoming Movies</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <slot></slot>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<script>
+</script>
+<style scoped>
+.header {
+  background-color: #F8F8F8;
+  border-color: #E7E7E7;
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+
+}
+
+</style>
